c284c53e3192730ac82fd8aa0c45c3b5dc0f3ce9
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-23 18:43:05 +0300

    Add Exercise 1

---
+++ 00_LanguageFundamentals/Exercise1/exc1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is comment */
+
+int main(int argc, char *argv[]){
+
+    int distance = 100;
+
+    // This is also comment
+
+    printf("You are %d miles away.", distance)
+
+    return 0;
+}
