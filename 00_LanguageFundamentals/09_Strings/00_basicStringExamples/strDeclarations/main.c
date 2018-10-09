cb48d18ef8b76a345fceb0b085c4beebfaaca20c
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-10-09 10:26:22 +0300

    str declarations

---
+++ 00_LanguageFundamentals/09_Strings/00_basicStringExamples/strDeclarations/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+	char str[4] = "mark";
+	int i;
+
+	for(i = 0; i < strlen(str); i++){
+			printf("%c\n", str[i]);
+			if(str[i] == '\0'){
+				printf("null");
+			}else if(str[i] == ' '){
+				printf("space");
+			}
+	}
+
+	printf("***********\n");
+
+	char myStr[5] = "mark";
+
+	for(i = 0; i < strlen(str); i++){
+			printf("%c\n", str[i]);
+			if(str[i] == '\0'){
+				printf("null");
+			}else if(str[i] == ' '){
+				printf("space");
+			}
+	}
+
+	printf("***********\n");
+
+
+	printf("%s\n",str);
+	printf("%s\n",myStr);
+
+
+}
