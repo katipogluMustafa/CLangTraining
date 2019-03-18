eaf99b3a56e45a957087db9ac604cb799cfc5139
Mustafa <mustafakatipoglu98@hotmail.com>
2019-03-18 10:35:41 -0700

    Update HeapSort, needs Additional Fix

--- 03_Data_Structures_And_Algorithms/Heap/main.c
+++ 03_Data_Structures_And_Algorithms/Heap/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<limits.h>
 
 /**
  * Inserts given value inside of array of heap tree with n elements, Mutates n since adds new element, Heap has new size
@@ -8,16 +9,16 @@
  */
 void insertMaxHeap(int HT[], int* n, int value){
   int i,parent;
-
   i = *n;
+  parent = (i - 1) / 2;
   HT[*n] = value;
   *n += 1;
-  parent = (i - 1) / 2;
   while( i >= 1 && value > HT[parent] ){
-    HT[i] = HT[parent]; 		
-    i = parent;
+    HT[i] = HT[parent];
+    i = parent; 		
+    parent = (i - 1) / 2;
   }
-  HT[i] = value; 		// insert the value
+  HT[parent] = value; 		// insert the value
 }
 
 /**
@@ -44,41 +45,26 @@ int findBiggestChild(int HT[], int i, int n){
  * Removes the root from the Max Heap
  *@param int HT[] Heap Tree
  *@param int n number of elements inside heap
- *@return int The deleted element's value which is the maximum element of the Heap
+ *@return int The deleted element's value which is the maximum element of the Heap, on Error returns INT_MIN
  */
 int removeMaxHeap(int HT[], int* n){
   if( *n == 0 ) 
-    return -1;
-  int tmp;
-  int max = HT[0];
+    return INT_MIN;
+  int tmp,max;
   *n -= 1;
+  max = HT[0];
   HT[0] = HT[*n];
   HT[*n] = max;		// put the deleted element max outside of the maxHeap's range
   int i = 0;
   int adr = findBiggestChild(HT, i, *n);
-  while( adr && (HT[i] > HT[adr] ) ){
+  while( adr && (HT[i] < HT[adr] ) ){
     tmp = HT[i];
     HT[i] = HT[adr];		// These 3 lines for swap(HT[i], HT[adr]);
     HT[adr] = tmp;
+    i = adr;
     adr = findBiggestChild(HT, i, *n);
   }
   return max;
-}
-
-/**
- * Create max heap with array of n elements
- * @param int A[] Array of elements
- * @param int n Number of elements
- */
-void createMaxHeap(int A[], int n){ 		
-  int i = 1;
-  while( i < n )
-    insertMaxHeap(A,&i,A[i]);		// i is incremented by insertMaxHeap function 
-}
-
-void heapSort(int A[], int n){
-  createMaxHeap(A,n);
-  while( removeMaxHeap(A,&n) != -1 );
 }
 
 void printArray(int* Arr, int n){
@@ -88,13 +74,38 @@ void printArray(int* Arr, int n){
   printf("\n");
 }
 
-int main(){
-  int Arr[] = {1,2,3,4,5,6,7,8,9,10};
-  printArray(Arr, 10);
-  createMaxHeap(Arr, 10);
-  printArray(Arr, 10);
-//  heapSort(Arr, 5);
-//  printArray(Arr, 5);
+/**
+ * Create max heap with array of n elements
+ * @param int A[] Array of elements
+ * @param int n Number of elements
+ */
+void createMaxHeap(int A[], int n){ 		
+  int i = 1;
+  while( i < n)
+    insertMaxHeap(A,&i,A[i]);		// i is incremented by insertMaxHeap function 
+  
+}
 
+void heapSort(int A[], int n){
+  int i;
+  int size = n;
+  createMaxHeap(A,n);
+
+  printArray(A,n); 
+  for(i = 0; i < n-1; i++){ 
+    removeMaxHeap(A,&size ); 
+    printArray(A,n); 
+  }
+
+//  createMaxHeap(A,n);
+//  while( removeMaxHeap(A, &n) != INT_MIN );
+}
+
+
+
+int main(){
+  int Arr[] = {10,9,8,7,6,5,4,3,2,1};
+  heapSort(Arr, 10);
+  printArray(Arr,10);
 return 0;
 }
